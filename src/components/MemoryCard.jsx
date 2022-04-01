847a62c6537d433c4874df55a5f9857cfc1b582b
rashida-otunba <rashidaotunba@gmail.com>
2022-04-01 19:40:13 -0400

    first push to git

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./MemoryCard.css"
+
+class MemoryCard extends React.Component {
+    
+    render() {
+        return (
+        
+            <div className="MemoryCard">
+                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
+            </div>
+            
+        )
+    }
+}
+
+export default MemoryCard;
