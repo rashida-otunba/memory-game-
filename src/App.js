483e2fbae552a03861aa44a102ebd1d3e7f01c03
rashida-otunba <rashidaotunba@gmail.com>
2022-04-02 19:21:31 -0400

    part-2 update for card flip logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,87 @@
+import React from "react";
 import "./App.css";
 import MemoryCard from "./components/MemoryCard";
+//editing app component will dictate how the child components render using props
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Memory Game</h1>
-        <h3 className="App-subtitle">
-          Match cards to win
-        </h3>
-      </header>
-      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
-      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
-      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
-      <div><MemoryCard /><MemoryCard /><MemoryCard /><MemoryCard /></div>
-    </div>
-    
-  );
+function generateDeck() {
+  const symbols = ["∆", "ß", "£", "§", "•", "$,", "+", "ø"];
+  const deck = [];
+  for (let i = 0; i < 16; i++) {
+    deck.push({
+      isFlipped: false,
+      symbol: symbols[i % 8],
+    });
+  }
+  shuffle(deck);
+  return deck;
+}
+
+function shuffle(deck) {
+  for (let i = deck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+    // console.log("deck 1", deck[i], "deck j:", deck[j]);
+  }
+  return deck;
+}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      deck: generateDeck(),
+      pickedCards: [],
+    };
+  }
+
+  pickCard(cardIndex) {
+    if (this.state.deck[cardIndex]) {
+      return;
+    }
+    const cardToFlip = { ...this.state.deck[cardIndex] };
+    cardToFlip.isFlipped = true;
+    const newPickedCards = this.state.pickedCards.concat(cardIndex);
+    const newDeck = this.state.deck.map((card, index) => {
+      if(cardIndex===index){
+        return cardToFlip;
+
+      }
+    return card;
+    });
+    this.setState(
+      {
+        deck: newDeck,
+        pickedCards: newPickedCards
+    }
+    )
+  }
+
+  render() {
+    let cardsJSX = this.state.deck.map((card, index) => {
+      return (
+        <MemoryCard
+          symbol={card.symbol}
+          isFlipped={card.isFlipped}
+          key={index}
+        />
+      );
+    });
+    console.log("cards!", cardsJSX);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Memory Game</h1>
+          <h3 className="App-subtitle">Match cards to win</h3>
+        </header>
+
+        <div>{cardsJSX.slice(0, 4)}</div>
+        <div>{cardsJSX.slice(4, 8)}</div>
+        <div>{cardsJSX.slice(8, 12)}</div>
+        <div>{cardsJSX.slice(12, 16)}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import "./MemoryCard.css"
+import React from "react";
+import "./MemoryCard.css";
 
 class MemoryCard extends React.Component {
-    
-    render() {
-        return (
-        
-            <div className="MemoryCard">
-                <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"></img>
-            </div>
-            
-        )
-    }
+  constructor(props){
+    super(props);
+    this.state = { isFlipped: false }; //this lets the card know, the default state is false 
+  }
+    clickHandler(){
+   this.setState({ isFlipped: !this.state.isFlipped });
+   }
+
+  render() {
+      let memoryCardInnerClass='MemoryCardInner';
+    return (
+      <div className="MemoryCard" 
+      onClick ={this.clickHandler.bind(this)}>
+        <div className={!this.props.isFlipped ? memoryCardInnerClass : "MemoryCardInner flipped"}>
+          <div className="MemoryCardBack">
+            <img
+              src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"
+              alt="card"
+            ></img>
+          </div>
+          <div className="MemoryCardFront">
+          {this.props.symbol}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MemoryCard;
